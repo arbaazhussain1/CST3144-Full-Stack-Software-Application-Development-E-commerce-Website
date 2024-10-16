30e93386ea38f903b9c44a0a2578c7311bd6d704
Arbaaz <arbaazahussain786@gmail.com>
2024-10-16 12:56:43 +0100

    Added new files which fixes the checkout button and placing some items in the checkout. Also, Separating new products to a new products.js file to allow future expansion

---
+++ public/JavaScript/products.js
@@ -0,0 +1,19 @@
+var products = [
+  {
+    id: 1,
+    title: "Product 1",
+    description: "Description of Product 1",
+    price: 100,
+    availableInventory: 5,
+    Image: "path/to/image1.jpg"
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    description: "Description of Product 2",
+    price: 150,
+    availableInventory: 3,
+    Image: "path/to/image2.jpg"
+  }
+  // More products...
+];
---
+++ public/products.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+      id: 1001,
+      title: "Maths lessons",
+      description: "Teaching lesson for students from Secondary schools from years 7 - 11 for 1 hour",
+      price: 100,
+      availableInventory: 10,
+      image: "/public/images1.jpeg",
+    },
+    {
+      id: 1002,
+      title: "English lessons",
+      description: "This is product 2",
+      price: 150,
+      availableInventory: 9,
+      image: "/public/images1.jpeg",
+    },
+    // Add more products...
+  ];
+  
